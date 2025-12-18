361ea364a7d77a82e0cc2f8c9a5310c049fb0720
daniaurica <danielaurica9@gmail.com>
2025-12-18 18:41:23 +0200

    Update candidate_register.cpp

--- src/candidate_register.cpp
+++ src/candidate_register.cpp
@@ -1,1 +1,39 @@
+#include "common.h"
 
+int main() {
+    // citim parametrii trimisi prin formular
+    map<string, string> params = readParams();
+
+    // construim un candidat din datele primite
+    Candidate c;
+    c.setNume(params["nume"]);
+    c.setPrenume(params["prenume"]);
+    c.setCnp(params["cnp"]);
+    c.setLiceu(params["liceu"]);
+    c.setFiliera(params["filiera"]);
+    c.setSpecializare(params["specializare"]);
+    c.setMedieGenerala(atof(params["medie_generala"].c_str()));
+    c.setNota1(atof(params["nota1"].c_str()));
+    c.setNota2(atof(params["nota2"].c_str()));
+
+    // calculam media de admitere
+    c.computeMediaAdmitere();
+
+    // salvam candidatul in fisier
+    bool ok = appendCandidateToFile(c);
+
+    // raspuns HTTP in format JSON
+    cout << "Content-type: application/json\r\n\r\n";
+
+    if (!ok) {
+        cout << "{ \"status\": \"error\", \"message\": \"nu pot scrie in fisier\" }\n";
+        return 0;
+    }
+
+    cout << "{\n";
+    cout << "  \"status\": \"ok\",\n";
+    cout << "  \"mediaAdmitere\": " << c.getMediaAdmitere() << "\n";
+    cout << "}\n";
+
+    return 0;
+}
